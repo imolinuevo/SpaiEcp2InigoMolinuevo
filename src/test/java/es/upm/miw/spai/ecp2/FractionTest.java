b69c06548568de666684ea3644ff9bb44c9d8d47
Sandra <Pitufilla@sandra>
2015-09-27 12:02:33 +0200

    Creado menor y test. Test ok

--- src/test/java/es/upm/miw/spai/ecp2/FractionTest.java
+++ src/test/java/es/upm/miw/spai/ecp2/FractionTest.java
@@ -2,33 +2,39 @@ package es.upm.miw.spai.ecp2;
 
 import static org.junit.Assert.*;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class FractionTest {
-	private Fraction fr;
-	
-	@Before
-	public void before(){
-		fr = new Fraction(6, 13);
-	}
+    private Fraction fr;
 
-	@Test
-	public void testFractionIntInt() {
-		assertEquals(6, fr.getNumerator());
-		assertEquals(13, fr.getDenominator());
-	}
+    @Before
+    public void before() {
+        fr = new Fraction(6, 13);
+    }
 
-	@Test
-	public void testFraction() {
-		fr = new Fraction();
-		assertEquals(1, fr.getNumerator());
-		assertEquals(1, fr.getDenominator());
-	}
+    @Test
+    public void testFractionIntInt() {
+        assertEquals(6, fr.getNumerator());
+        assertEquals(13, fr.getDenominator());
+    }
 
-	@Test
-	public void testDecimal() {
-		assertEquals(0.46, fr.decimal(), 0.01);
-	}
+    @Test
+    public void testFraction() {
+        fr = new Fraction();
+        assertEquals(1, fr.getNumerator());
+        assertEquals(1, fr.getDenominator());
+    }
 
+    @Test
+    public void testDecimal() {
+        assertEquals(0.46, fr.decimal(), 0.01);
+    }
+
+    @Test
+    public void testMenor() {
+        Fraction fr1 = new Fraction(1, 9);
+        Assert.assertEquals(fr.menor(fr1), fr);
+    }
 }
